a6d019b31adc0b58c7bf063094a4752749575187
Deep Patel <97653978+coderkage@users.noreply.github.com>
2024-05-19 11:18:59 +0530

    fixed issues

--- app.py
+++ app.py
@@ -40,48 +40,87 @@ users = ['Deep', 'Prayas', 'Shivanshu']
 task_files = {user: f"{user}_tasks.csv" for user in users}
 
 def is_submission_allowed(user):
-    current_time = datetime.now()
-    start_time_today = current_time.replace(hour=5, minute=30, second=0, microsecond=0)
+    current_date = datetime.now().date()
     
-    if start_time_today <= current_time:
-        if user in task_files:
-            if os.path.exists(task_files[user]):
-                last_submission_time = pd.read_csv(task_files[user])['Submission Time'].max()
-                if pd.isnull(last_submission_time) or pd.to_datetime(last_submission_time) < start_time_today:
+    if user in task_files:
+        task_file_path = task_files[user]
+        
+        if os.path.exists(task_file_path):
+            submission_times = pd.read_csv(task_file_path)['Submission Time']
+            
+            if not submission_times.empty:
+                last_submission_time = pd.to_datetime(submission_times).max()
+                if last_submission_time.date() != current_date:
                     return True
+            else:
+                return False
     return False
 
+def count_streak(task_file):
+    if os.path.exists(task_file):
+            task_df = pd.read_csv(task_file)
+            task_df['Submission Time'] = pd.to_datetime(task_df['Submission Time'])
+            task_df = task_df.sort_values(by='Submission Time', ascending=False)
+
+            current_time = datetime.now()
+            today = current_time.date()
+            yesterday = today - timedelta(days=1)
+
+            streak_count = 0
+            for submission_time in task_df['Submission Time']:
+                submission_date = submission_time.date()
+                if streak_count == 0:
+                    if submission_date in [today, yesterday]:
+                        streak_count = 1
+                        current_streak_day = submission_date
+                    else:
+                        break
+                else:
+                    if current_streak_day - timedelta(days=streak_count) == submission_date:
+                        streak_count += 1
+                    else:
+                        break
+    return streak_count
 
 def main():
-    st.title('75-Hard Challenge : Geek Edition🤓')
+    st.title('75-Hard Challenge: Geek Edition🤓')
+
+    current_time = datetime.now()
+    start_time_today = current_time.replace(hour=0, minute=0, second=0)
+    end_time_today = current_time.replace(hour=23, minute=59, second=59)
+    time_left = end_time_today - current_time
+
+    st.info(f"Time left to submit today\'s work: {time_left} hours")
+
+
     for user, task_file in task_files.items():
         if os.path.exists(task_file):
             last_submission_time = pd.read_csv(task_file)['Submission Time'].max()
-            if pd.isnull(last_submission_time) or (datetime.now() - pd.to_datetime(last_submission_time)) >= timedelta(days=1):
-                st.error(f"{user} broke the streak!") 
-           
-    current_time = datetime.now()
-    start_time_today = current_time.replace(hour=5, minute=30, second=0)
-    end_time_today = (current_time + timedelta(days=1)).replace(hour=5, minute=29, second=59)
-    time_left = end_time_today - current_time
-    
-    st.info(f"Time left to submit today's work: {time_left}")
+            if pd.isnull(last_submission_time):
+                st.warning(f"{user} haven't started the challenege yet!")
+            else:
+                last_submission_date = pd.to_datetime(last_submission_time).date()
+                today = datetime.now().date()
+                yesterday = today - timedelta(days=1)
+                if last_submission_date != today and last_submission_date != yesterday:
+                    st.error(f"{user} broke the streak!")
+ 
     st.subheader("Streak Progress")
     for user, task_file in task_files.items():
-        if os.path.exists(task_file):
-            task_df = pd.read_csv(task_file)
-            streak_count = len(task_df)
-            st.write(f"{user}: {streak_count}")
+        streak_count = count_streak(task_file)
 
-            if streak_count >= 75:
-                st.success(f"Congratulations! {user} has achieved a 75-day streak!")
-                st.progress(1.0)
-            else:
-                progress = streak_count / 75
-                st.progress(progress)
+        st.write(f"{user}: {streak_count}")
+
+        if streak_count >= 75:
+            st.success(f"Congratulations! {user} has achieved a 75-day streak!")
+            st.progress(1.0)
+        else:
+            progress = streak_count / 75
+            st.progress(progress)
+
 
     selected_user = st.selectbox('Select User:', users)
-    task = st.text_input('Write your today\'s work:')
+    task = st.text_input('Write your task:')
     
     if selected_user and task:
         if st.button('Submit'):
@@ -109,27 +148,28 @@ def main():
         if os.path.exists(task_file):
             task_df = pd.read_csv(task_file)
             last_task_index = len(task_df) - 1
-            last_task_text = task_df.iloc[last_task_index]['Task']
-            with st.expander(f"Edit Last Task for {selected_user}"):
-                edited_task = st.text_input(f'Edit Work:', value=last_task_text, key=f"edit_{selected_user}")
-                if st.button(f'Update Last Work for {selected_user}'):
-                    task_df.at[last_task_index, 'Task'] = edited_task
-                    task_df.to_csv(task_file, index=False)
-                    st.success('Work updated successfully!')
+            if last_task_index != -1:
+                last_task_text = task_df.iloc[last_task_index]['Task']
+                with st.expander(f"Edit Last Task for {selected_user}"):
+                    edited_task = st.text_input(f'Edit Work:', value=last_task_text, key=f"edit_{selected_user}")
+                    if st.button(f'Update Last Work for {selected_user}'):
+                        task_df.at[last_task_index, 'Task'] = edited_task
+                        task_df.to_csv(task_file, index=False)
+                        st.success('Work updated successfully!')
+            else:            
+                st.warning('The task list is empty!')
 
 
     st.subheader("Streak Counts")
     progress_html1 = f""" <hr>  """
     st.markdown(progress_html1, unsafe_allow_html=True)
     for user, task_file in task_files.items():
-        if os.path.exists(task_file):
-            task_df = pd.read_csv(task_file)
-            streak_count = len(task_df)
+        streak_count = count_streak(task_file)
             # st.write(f"{user}: {streak_count}/75")
 
-            progress = min(streak_count / 75, 1.0)
+        progress = min(streak_count / 75, 1.0)
             
-            progress_html = f"""
+        progress_html = f"""
             
                 <div class="circular-progress-container">
                     <div class="progress-name">{user}</div>
@@ -145,7 +185,7 @@ def main():
                 </div>
                 <hr>
             """
-            st.markdown(progress_html, unsafe_allow_html=True)
+        st.markdown(progress_html, unsafe_allow_html=True)
 
     
                 
@@ -189,7 +229,7 @@ def main():
                 transform: translate(-50%, -50%);
                 font-size: 50px;
                 font-weight: bold;
-                color: #f06624; /* orange color */
+                color: #f06624;
                 opacity: 80%;
             }
 
@@ -200,7 +240,7 @@ def main():
                 transform: translate(-50%, -50%);
                 font-size: 25px;
                 font-weight: bold;
-                color: #000000; /* black color */
+                color: #000000;
                 opacity: 75%;
             }
         </style>
@@ -209,23 +249,25 @@ def main():
     )
     
     footer_html = """
-<style>
-.footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px 0;
-}
-</style>
 
-<div class="footer">
-    <p>Made with ❤️ by Deep Patel.</p>
-</div>
-"""
+        <div class="footer">
+            <p>Made with ❤️ by Deep Patel.</p>
+        </div>
+
+        <style>
+            .footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: #333;
+                color: #fff;
+                text-align: center;
+                padding: 10px 0;
+            }
+        </style>
+
+        """
 
 # Display footer
     st.markdown(footer_html, unsafe_allow_html=True)
